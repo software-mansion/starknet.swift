b4362aec7914366b677d6f175754b54412a12e9f
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2022-11-24 18:01:53 +0100

    Keccak implementation and selector calculation

--- Package.swift
+++ Package.swift
@@ -18,14 +18,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0")
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.6.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Starknet",
-            dependencies: ["BigInt"]),
+            dependencies: ["BigInt", "CryptoSwift"]),
         .testTarget(
             name: "StarknetTests",
             dependencies: ["Starknet", "BigInt"]),
