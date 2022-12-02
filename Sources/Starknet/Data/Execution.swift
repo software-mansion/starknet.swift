6db0ab3ed00fd11ed88ec4808b19290e0a79f8db
David Rodrigues <david.ndh@gmail.com>
2022-12-02 10:49:49 +0000

    Add missing public initialiser for `StarknetCall` (#19)

--- Sources/Starknet/Data/Execution.swift
+++ Sources/Starknet/Data/Execution.swift
@@ -7,6 +7,12 @@ public struct StarknetCall: Codable {
     public let entrypoint: Felt
     public let calldata: StarknetCalldata
     
+    public init(contractAddress: Felt, entrypoint: Felt, calldata: StarknetCalldata) {
+        self.contractAddress = contractAddress
+        self.entrypoint = entrypoint
+        self.calldata = calldata
+    }
+
     enum CodingKeys: String, CodingKey {
         case contractAddress = "contract_address"
         case entrypoint = "entry_point_selector"
