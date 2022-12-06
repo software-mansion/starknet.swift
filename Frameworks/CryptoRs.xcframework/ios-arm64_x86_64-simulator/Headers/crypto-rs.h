268d14964e8c1b145e1c1e7dab1ae297a2e670d7
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2022-12-06 17:39:14 +0100

    Updated CryptoRs xcframework

--- Frameworks/CryptoRs.xcframework/ios-arm64_x86_64-simulator/Headers/crypto-rs.h
+++ Frameworks/CryptoRs.xcframework/ios-arm64_x86_64-simulator/Headers/crypto-rs.h
@@ -7,5 +7,3 @@ int32_t generate_k(const uint8_t *p_message_hash,
                    const uint8_t *p_private_key,
                    const uint8_t *p_seed,
                    uint8_t *out_buffer);
-
-int32_t starknet_keccak(const uint8_t *p_data, uint32_t p_data_size, uint8_t *out_buffer);
