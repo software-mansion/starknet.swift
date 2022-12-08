57b6d43b2247569a24880f576f2a76e26717831c
David Rodrigues <david@argent.xyz>
2022-12-08 09:58:24 +0000

    Add testnet2 chain (#21)

--- Sources/Starknet/Data/StarknetChainId.swift
+++ Sources/Starknet/Data/StarknetChainId.swift
@@ -4,6 +4,7 @@ import BigInt
 public enum StarknetChainId {
     case mainnet
     case testnet
+    case testnet2
     
     public var feltValue: Felt {
         switch self {
@@ -11,6 +12,8 @@ public enum StarknetChainId {
             return Felt(fromHex: "0x534e5f4d41494e")!
         case .testnet:
             return Felt(fromHex: "0x534e5f474f45524c49")!
+        case .testnet2:
+            return Felt(fromHex: "0x534e5f474f45524c4932")!
         }
     }
 }
