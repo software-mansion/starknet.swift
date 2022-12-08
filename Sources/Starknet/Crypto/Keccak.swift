11f55c9dabde634a5b76a9e44c1811f79f7a1c7a
David Rodrigues <david@argent.xyz>
2022-12-08 14:43:57 +0000

    Make `keccak` public to allow sign arbitrary messages

--- Sources/Starknet/Crypto/Keccak.swift
+++ Sources/Starknet/Crypto/Keccak.swift
@@ -4,7 +4,7 @@ import BigInt
 
 private let mask250 = BigUInt(2).power(250) - BigUInt(1)
 
-internal func keccak(on bytes: [UInt8]) -> Felt {
+public func keccak(on bytes: [UInt8]) -> Felt {
     let hashed = bytes.sha3(.keccak256)
     let data = Data(hashed)
     let masked = BigUInt(data) & mask250
