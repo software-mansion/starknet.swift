ee61a071205fe054726e1b29d3266dba0fcd8742
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2022-12-20 13:15:37 +0100

    Updated tests

--- Tests/StarknetTests/Data/TransactionTests.swift
+++ Tests/StarknetTests/Data/TransactionTests.swift
@@ -28,7 +28,7 @@ final class TransactionTests: XCTestCase {
     
     func testInvokeTransactionDecoding() throws {
         let json = """
-        {"sender_address":"0x123","calldata":["0x1","0x2"],"max_fee":"0x859","signature":["0x1","0x2"],"nonce":"0x0","type":"invoke","version":"0x1"}
+        {"sender_address":"0x123","calldata":["0x1","0x2"],"max_fee":"0x859","signature":["0x1","0x2"],"nonce":"0x0","type":"INVOKE","version":"0x1"}
         """.data(using: .utf8)!
         
         let decoder = JSONDecoder()
@@ -36,13 +36,13 @@ final class TransactionTests: XCTestCase {
         XCTAssertNoThrow(try decoder.decode(StarknetSequencerInvokeTransaction.self, from: json))
         
         let json2 = """
-        {"sender_address":"0x123","calldata":["0x1","0x2"],"max_fee":"0x859","signature":["0x1","0x2"],"nonce":"0x0","type":"deploy","version":"0x1"}
+        {"sender_address":"0x123","calldata":["0x1","0x2"],"max_fee":"0x859","signature":["0x1","0x2"],"nonce":"0x0","type":"DEPLOY","version":"0x1"}
         """.data(using: .utf8)!
         
         XCTAssertThrowsError(try decoder.decode(StarknetSequencerInvokeTransaction.self, from: json2))
         
         let json3 = """
-        {"sender_address":"0x123","calldata":["0x1","0x2"],"max_fee":"0x859","signature":["0x1","0x2"],"nonce":"0x0","type":"invoke","version":"0x0"}
+        {"sender_address":"0x123","calldata":["0x1","0x2"],"max_fee":"0x859","signature":["0x1","0x2"],"nonce":"0x0","type":"INVOKE","version":"0x0"}
         """.data(using: .utf8)!
         
         XCTAssertThrowsError(try decoder.decode(StarknetSequencerInvokeTransaction.self, from: json3))
