ba6fff26a3fbe987b97d7430c59a7c817c6ee821
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2023-02-03 18:32:28 +0100

    Added comments

--- Examples/StarknetDemo/StarknetDemo/AccountsStore.swift
+++ Examples/StarknetDemo/StarknetDemo/AccountsStore.swift
@@ -9,19 +9,28 @@ import BigInt
 import Foundation
 import Starknet
 
-let erc20: Felt = "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7"
+// Address of the native erc20 contract in starknet-devnet.
+let erc20ContractAddress: Felt = "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7"
 
-func makeAccount(provider: StarknetProviderProtocol, privateKey: Felt, address: Felt) -> StarknetAccountProtocol {
-    let signer = StarkCurveSigner(privateKey: privateKey)!
+// In this demo, you can interact with two predeployed accounts.
+// Please run starknet-devnet as follows:
+// starknet-devnet --port 5050 --seed 0
+// to be able to use the accounts defined below.
+// If you used a different seed update private keys and account addresses accordingly.
+// If you used a different port, please change it below as well.
 
-    let account = StarknetAccount(
-        address: address,
-        signer: signer,
-        provider: provider
-    )
+// Address to the the rpc endpoint of a local devnet instance
+let rpcEndpoint = "http://localhost:5050/rpc"
 
-    return account
-}
+// !!! Important !!!
+// These private keys are for demo only. Never publish a privateKey of your wallet, nor
+// store it in any repository as plain text.
+let account1PrivateKey: Felt = "0xe3e70682c2094cac629f6fbed82c07cd"
+let account2PrivateKey: Felt = "0xf728b4fa42485e3a0a5d2f346baa9455"
+
+// Addresses of accounts associated with above private keys.
+let account1Address: Felt = "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a"
+let account2Address: Felt = "0x69b49c2cc8b16e80e86bfc5b0614a59aa8c9b601569c7b80dde04d3f3151b79"
 
 class AccountsStore: ObservableObject {
     let accounts: [StarknetAccountProtocol]
@@ -48,19 +57,27 @@ class AccountsStore: ObservableObject {
     }
 
     init() {
-        let provider = StarknetProvider(starknetChainId: .testnet, url: "http://localhost:5050/rpc")!
-        self.provider = provider
+        // Create starknet provider that will be used to communicate with the given starknet node.
+        self.provider = StarknetProvider(starknetChainId: .testnet, url: rpcEndpoint)!
 
-        let account1 = makeAccount(
-            provider: provider,
-            privateKey: "0xe3e70682c2094cac629f6fbed82c07cd",
-            address: "0x7e00d496e324876bbc8531f2d9a82bf154d1a04a50218ee74cdd372f75a551a"
+        // Create a signer that will be used to sign starknet transactions with provided private key.
+        let account1Signer = StarkCurveSigner(privateKey: account1PrivateKey)!
+
+        // With address, signer and provider you can create starknet account.
+        // Please note that it will only work if it's already deployed.
+        let account1 = StarknetAccount(
+            address: account1Address,
+            signer: account1Signer,
+            provider: provider
         )
 
-        let account2 = makeAccount(
-            provider: provider,
-            privateKey: "0xf728b4fa42485e3a0a5d2f346baa9455",
-            address: "0x69b49c2cc8b16e80e86bfc5b0614a59aa8c9b601569c7b80dde04d3f3151b79"
+        // And do the same for the second account.
+        let account2Signer = StarkCurveSigner(privateKey: account2PrivateKey)!
+
+        let account2 = StarknetAccount(
+            address: account2Address,
+            signer: account2Signer,
+            provider: provider
         )
 
         self.accounts = [account1, account2]
@@ -70,15 +87,19 @@ class AccountsStore: ObservableObject {
     func fetchBalance() async {
         let accountIndex = currentAccountIndex
 
+        // Prepare a read call to be sent to starknet
         let call = StarknetCall(
-            contractAddress: erc20,
+            contractAddress: erc20ContractAddress,
             entrypoint: starknetSelector(from: "balanceOf"),
             calldata: [account.address]
         )
 
         do {
+            // Note that as it is a read only call, you can send it using provider.
             let result = try await provider.callContract(call)
 
+            // This erc20 contract uses uint256 instead of felt for balances, which is stored
+            // as two felts - lower and upper 128 bits of the uint256.
             let balanceValue = result[0].value + result[1].value << 128
 
             DispatchQueue.main.async {
@@ -106,7 +127,7 @@ class AccountsStore: ObservableObject {
         let recipientAddress = accounts[recipientAccoutnIndex].address
 
         let call = StarknetCall(
-            contractAddress: erc20,
+            contractAddress: erc20ContractAddress,
             entrypoint: starknetSelector(from: "transfer"),
             calldata: [recipientAddress, low.toFelt()!, high.toFelt()!]
         )
@@ -115,6 +136,7 @@ class AccountsStore: ObservableObject {
 
         Task {
             let _ = try await senderAccount.execute(call: call)
+
             try await Task.sleep(nanoseconds: UInt64(Double(NSEC_PER_SEC)))
 
             await fetchBalance()
