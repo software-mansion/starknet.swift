1af395580c40aad42d199050ae360ebf326702bd
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2023-02-08 15:15:54 +0100

    Added contracts to resources

---
+++ Tests/StarknetTests/Resources/compileContracts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit
+mkdir -p "Compiled"
+
+while IFS= read -r -d '' file; do
+  name="$(basename -- "$file" .cairo)"
+  if [[ $name == *"account"* ]]; then
+    starknet-compile "$file" --account_contract --output "Compiled/$name.json" --abi "Compiled/${name}Abi.json"
+  else
+    starknet-compile "$file" --output "Compiled/$name.json" --abi "Compiled/${name}Abi.json"
+  fi
+done < <(find Contracts -name "*.cairo" -type f -print0)
+
