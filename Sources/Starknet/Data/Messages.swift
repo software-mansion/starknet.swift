7011dec1171b4ed968705bb5c9e5a3b509dab5be
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-07-27 15:57:54 +0200

    Update message dataclasses (0.4.0-rc2)

--- Sources/Starknet/Data/Messages.swift
+++ Sources/Starknet/Data/Messages.swift
@@ -1,22 +1,6 @@
 import Foundation
 
 public struct MessageToL1: Decodable, Equatable {
-    public let toAddress: Felt
-    public let payload: Felt
-
-    enum CodingKeys: String, CodingKey {
-        case toAddress = "to_address"
-        case payload
-    }
-}
-
-/// Message from L1
-///
-/// - Parameters:
-///  - fromAddress: The address of the L1 contract sending the message
-///  - toAddress: The target L2 address the message is sent to
-///  - payload: The payload of the message
-public struct MessageFromL1: Codable, Equatable {
     public let fromAddress: Felt
     public let toAddress: Felt
     public let payload: [Felt]
@@ -24,6 +8,20 @@ public struct MessageFromL1: Codable, Equatable {
     enum CodingKeys: String, CodingKey {
         case fromAddress = "from_address"
         case toAddress = "to_address"
+        case payload
+    }
+}
+
+public struct MessageFromL1: Codable, Equatable {
+    public let fromAddress: Felt
+    public let toAddress: Felt
+    public let selector: Felt
+    public let payload: [Felt]
+
+    enum CodingKeys: String, CodingKey {
+        case fromAddress = "from_address"
+        case toAddress = "to_address"
+        case selector = "entry_point_selector"
         case payload
     }
 }
