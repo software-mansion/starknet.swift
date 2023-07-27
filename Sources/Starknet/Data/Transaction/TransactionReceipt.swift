33915bc69a904a53ef2bee8ee07b5cd0a511e86e
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-07-27 15:59:45 +0200

    Add revert reason, finality and execution status

--- Sources/Starknet/Data/Transaction/TransactionReceipt.swift
+++ Sources/Starknet/Data/Transaction/TransactionReceipt.swift
@@ -3,11 +3,13 @@ import Foundation
 public struct StarknetCommonTransactionReceipt: StarknetTransactionReceipt, Decodable {
     public let transactionHash: Felt
     public let actualFee: Felt
-    public let status: StarknetTransactionStatus
+    public let executionStatus: StarknetTransactionExecutionStatus
+    public let finalityStatus: StarknetTransactionFinalityStatus
     public let blockHash: Felt
     public let blockNumber: UInt64
     public let type: StarknetTransactionReceiptType
     public let messagesSent: [MessageToL1]
+    public let revertReason: String?
     public let events: [StarknetEvent]
     public let contractAddress: Felt?
 
@@ -18,7 +20,9 @@ public struct StarknetCommonTransactionReceipt: StarknetTransactionReceipt, Deco
         case blockNumber = "block_number"
         case messagesSent = "messages_sent"
         case contractAddress = "contract_address"
-        case status
+        case executionStatus = "execution_status"
+        case finalityStatus = "finality_status"
+        case revertReason = "revert_reason"
         case type
         case events
     }
@@ -27,9 +31,12 @@ public struct StarknetCommonTransactionReceipt: StarknetTransactionReceipt, Deco
 public struct StarknetPendingTransactionReceipt: StarknetTransactionReceipt, Decodable {
     public let transactionHash: Felt
     public let actualFee: Felt
+    public let executionStatus: StarknetTransactionExecutionStatus
+    public let finalityStatus: StarknetTransactionFinalityStatus
     public let type: StarknetTransactionReceiptType? = .pending
     public let messagesSent: [MessageToL1]
     public let events: [StarknetEvent]
+    public let revertReason: String?
     public let contractAddress: Felt?
 
     enum CodingKeys: String, CodingKey {
@@ -37,6 +44,9 @@ public struct StarknetPendingTransactionReceipt: StarknetTransactionReceipt, Dec
         case actualFee = "actual_fee"
         case messagesSent = "messages_sent"
         case contractAddress = "contract_address"
+        case executionStatus = "execution_status"
+        case finalityStatus = "finality_status"
+        case revertReason = "revert_reason"
         case type
         case events
     }
--- Sources/Starknet/Data/Transaction/TransactionStatus.swift
+++ Sources/Starknet/Data/Transaction/TransactionStatus.swift
@@ -10,3 +10,21 @@ public enum StarknetTransactionStatus: String, Codable {
         Felt.fromShortString(self.rawValue.lowercased())!
     }
 }
+
+public enum StarknetTransactionExecutionStatus: String, Codable {
+    case succeeded = "SUCCEEDED"
+    case reverted = "REVERTED"
+
+    public var encodedValue: Felt {
+        Felt.fromShortString(self.rawValue.lowercased())!
+    }
+}
+
+public enum StarknetTransactionFinalityStatus: String, Codable {
+    case acceptedL1 = "ACCEPTED_ON_L1"
+    case acceptedL2 = "ACCEPTED_ON_L2"
+
+    public var encodedValue: Felt {
+        Felt.fromShortString(self.rawValue.lowercased())!
+    }
+}
