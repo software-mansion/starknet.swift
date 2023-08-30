712aa8907c9f6555b3a6d5f2901a76a47fd54e0c
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-08-30 18:32:20 +0200

    Move StarknetSequencerTransaction extension to SequencerTransaction.swift

--- Sources/Starknet/Data/Transaction/Transaction.swift
+++ Sources/Starknet/Data/Transaction/Transaction.swift
@@ -425,23 +425,3 @@ public enum StarknetTransactionDecodingError: Error {
     case invalidVersion
     case invalidType
 }
-
-// Default deserializer doesn't check if the fields with default values match what is deserialized.
-// It's an extension that resolves this.
-internal extension StarknetSequencerTransaction {
-    func verifyTransactionType<T>(container: KeyedDecodingContainer<T>, codingKeysType _: T.Type) throws where T: CodingKey {
-        let type = try container.decode(StarknetTransactionType.self, forKey: T(stringValue: "type")!)
-
-        guard type == self.type else {
-            throw StarknetTransactionDecodingError.invalidType
-        }
-    }
-
-    func verifyTransactionVersion<T>(container: KeyedDecodingContainer<T>, codingKeysType _: T.Type) throws where T: CodingKey {
-        let version = try container.decode(Felt.self, forKey: T(stringValue: "version")!)
-
-        guard version == self.version else {
-            throw StarknetTransactionDecodingError.invalidVersion
-        }
-    }
-}
