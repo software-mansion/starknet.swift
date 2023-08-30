c209afde2d7b53d15b630fd9676adab11694820e
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-08-30 18:39:22 +0200

    Remove old tx status

--- Sources/Starknet/Data/Transaction/TransactionStatus.swift
+++ Sources/Starknet/Data/Transaction/TransactionStatus.swift
@@ -1,16 +1,5 @@
 import Foundation
 
-public enum StarknetTransactionStatus: String, Codable {
-    case pending = "PENDING"
-    case rejected = "REJECTED"
-    case acceptedL1 = "ACCEPTED_ON_L1"
-    case acceptedL2 = "ACCEPTED_ON_L2"
-
-    public var encodedValue: Felt {
-        Felt.fromShortString(self.rawValue.lowercased())!
-    }
-}
-
 public enum StarknetTransactionExecutionStatus: String, Codable {
     case succeeded = "SUCCEEDED"
     case reverted = "REVERTED"
