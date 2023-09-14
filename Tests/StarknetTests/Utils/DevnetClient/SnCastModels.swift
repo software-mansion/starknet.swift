2e30f8cd2dc7011d72bfb085317a9327b4633c07
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-09-14 11:59:36 +0200

    Add SnCast models; Add SnCastResponseWrapper

---
+++ Tests/StarknetTests/Utils/DevnetClient/SnCastModels.swift
@@ -0,0 +1,60 @@
+import BigInt
+import Foundation
+import Starknet
+
+struct AccountDeploySnCastResponse: SnCastResponse {
+    let command: SnCastCommand = .accountDeploy
+    let error: String? = nil
+    let transactionHash: Felt
+
+    enum CodingKeys: String, CodingKey {
+        case transactionHash = "transaction_hash"
+    }
+}
+
+struct AccountCreateSnCastResponse: SnCastResponse {
+    let command: SnCastCommand = .accountCreate
+    let error: String? = nil
+    let accountAddress: Felt
+    let maxFee: Felt
+
+    enum CodingKeys: String, CodingKey {
+        case accountAddress = "address"
+        case maxFee = "max_fee"
+    }
+}
+
+struct DeclareSnCastResponse: SnCastResponse {
+    let command: SnCastCommand = .declare
+    let error: String? = nil
+    let classHash: Felt
+    let transactionHash: Felt
+
+    enum CodingKeys: String, CodingKey {
+        case classHash = "class_hash"
+        case transactionHash = "transaction_hash"
+    }
+}
+
+struct DeploySnCastResponse: SnCastResponse {
+    let command: SnCastCommand = .deploy
+    let error: String? = nil
+    let contractAddress: Felt
+    let transactionHash: Felt
+   
+    enum CodingKeys: String, CodingKey {
+        case contractAddress = "contract_address"
+        case transactionHash = "transaction_hash"
+    }
+}
+
+protocol SnCastResponse: Decodable {
+    var command: SnCastCommand { get }
+    var error: String? { get }
+}
+
+public enum SnCastError: Error {
+    case snCastError(String)
+    case commandError(String)
+    case invalidResponseJson
+}
---
+++ Tests/StarknetTests/Utils/DevnetClient/SnCastResponseWrapper.swift
@@ -0,0 +1,57 @@
+import BigInt
+import Foundation
+@testable import Starknet
+
+enum SnCastCommand: String, Codable {
+    case accountCreate = "account create"
+    case accountDeploy = "account deploy"
+    case declare
+    case deploy
+}
+
+enum SnCastResponseWrapper: Decodable {
+    fileprivate enum Keys: String, CodingKey {
+        case command
+        case error
+    }
+
+    case accountCreate(AccountCreateSnCastResponse)
+    case accountDeploy(AccountDeploySnCastResponse)
+    case declare(DeclareSnCastResponse)
+    case deploy(DeploySnCastResponse)
+
+    public var response: any SnCastResponse {
+        switch self {
+        case let .accountCreate(res):
+            return res
+        case let .accountDeploy(res):
+            return res
+        case let .declare(res):
+            return res
+        case let .deploy(res):
+            return res
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Keys.self)
+        let command = try container.decode(SnCastCommand.self, forKey: .command)
+        let error = try container.decodeIfPresent(String.self, forKey: .error)
+        guard error == nil else {
+            throw SnCastError.commandError(error!)
+        }
+
+        switch command {
+        case .accountCreate:
+            self = try .accountCreate(AccountCreateSnCastResponse(from: decoder))
+        case .accountDeploy:
+            self = try .accountDeploy(AccountDeploySnCastResponse(from: decoder))
+        case .declare:
+            self = try .declare(DeclareSnCastResponse(from: decoder))
+        case .deploy:
+            self = try .deploy(DeploySnCastResponse(from: decoder))
+        default:
+            throw SnCastError.snCastError("Invalid command response Json.")
+        }
+    }
+}
