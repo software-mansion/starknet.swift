e2d78d14684d3f95b4b5109bf6584cc3606fcb3c
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-10-16 19:24:41 +0200

    Format

--- Tests/StarknetTests/Providers/ProviderTests.swift
+++ Tests/StarknetTests/Providers/ProviderTests.swift
@@ -30,7 +30,7 @@ final class ProviderTests: XCTestCase {
     func makeStarknetProvider(url: String) -> StarknetProviderProtocol {
         StarknetProvider(starknetChainId: .testnet, url: url)!
     }
-    
+
     // TODO: Re-enable when devnet-rs supports RPC 0.5.0
     func disabledTestSpecVersion() async throws {
         let result = try await provider.specVersion()
