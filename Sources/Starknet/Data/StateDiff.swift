35d73fa3a83d02193326a2bac624c996fa1b6e36
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-10-18 13:34:40 +0200

    Add StateDiff

---
+++ Sources/Starknet/Data/StateDiff.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public struct StarknetStateDiff: Decodable, Equatable {
+    public let storageDiffs: [StarknetStorageDiffItem]
+    public let deprecatedDeclaredClasses: [Felt]
+    public let declaredClasses: [StarknetDeclaredClassItem]
+    public let deployedContracts: [StarknetDeployedContractItem]
+    public let replacedClasses: [StarknetReplacedClassItem]
+    public let nonces: [StarknetNonceUpdateItem]
+
+    enum CodingKeys: String, CodingKey {
+        case storageDiffs = "storage_diffs"
+        case deprecatedDeclaredClasses = "deprecated_declared_classes"
+        case declaredClasses = "declared_classes"
+        case deployedContracts = "deployed_contracts"
+        case replacedClasses = "replaced_classes"
+        case nonces
+    }
+}
