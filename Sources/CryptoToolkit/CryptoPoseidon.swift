594a6f162f0a24e21ae3352b9994b43384dfaf45
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-01-02 19:08:09 +0100

    Make CryptoPoseidon a class

--- Sources/CryptoToolkit/CryptoPoseidon.swift
+++ Sources/CryptoToolkit/CryptoPoseidon.swift
@@ -1,7 +1,7 @@
 import CFrameworkWrapper
 import Foundation
 
-public enum CryptoPoseidon {
+public class CryptoPoseidon {
     public static func hades(
         _ values: [(UInt64, UInt64, UInt64, UInt64)]
     ) -> [(UInt64, UInt64, UInt64, UInt64)] {
