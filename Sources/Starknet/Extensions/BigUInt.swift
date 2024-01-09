ec8ec914bbb1606e7598ac57361d64bf637b40bf
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-01-09 19:22:21 +0100

    Format

--- Sources/Starknet/Extensions/BigUInt.swift
+++ Sources/Starknet/Extensions/BigUInt.swift
@@ -9,7 +9,7 @@ public extension BigUInt {
     func toFeltClamped() -> Felt {
         self > Felt.max.value ? Felt.max : Felt(self)!
     }
-    
+
     func toNumAsHex() -> NumAsHex? {
         NumAsHex(self)
     }
