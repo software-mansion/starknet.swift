97127a7775785988d27ac4f368c7600a5b800cff
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-01-09 19:22:07 +0100

    Add public constructors to StarknetResourceBoundsMapping and StarknetResourceBounds

--- Sources/Starknet/Data/Transaction/Data/ResourceBounds.swift
+++ Sources/Starknet/Data/Transaction/Data/ResourceBounds.swift
@@ -1,8 +1,18 @@
 import Foundation
 
-public struct StarknetResourceBoundsResourceBounds {
-    public let maxAmount: UInt64
+public struct StarknetResourceBounds: Codable, Equatable, Hashable {
+    public let maxAmount: UInt64AsHex
     public let maxPricePerUnit: UInt128AsHex
+
+    public static let zero = StarknetResourceBounds(
+        maxAmount: .zero,
+        maxPricePerUnit: .zero
+    )
+
+    public init(maxAmount: UInt64AsHex, maxPricePerUnit: UInt128AsHex) {
+        self.maxAmount = maxAmount
+        self.maxPricePerUnit = maxPricePerUnit
+    }
 
     enum CodingKeys: String, CodingKey {
         case maxAmount = "max_amount"
@@ -10,9 +20,14 @@ public struct StarknetResourceBoundsResourceBounds {
     }
 }
 
-public struct StarknetResourceBoundsResourceBoundsMapping {
-    public let l1Gas: StarknetResourceBoundsResourceBounds
-    public let l2Gas: StarknetResourceBoundsResourceBounds
+public struct StarknetResourceBoundsMapping: Codable, Equatable, Hashable {
+    public let l1Gas: StarknetResourceBounds
+    public let l2Gas: StarknetResourceBounds
+
+    public init(l1Gas: StarknetResourceBounds) {
+        self.l1Gas = l1Gas
+        self.l2Gas = StarknetResourceBounds.zero
+    }
 
     enum CodingKeys: String, CodingKey {
         case l1Gas = "l1_gas"
