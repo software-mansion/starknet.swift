984c44ba83d59275c923ba00bea4159886f7eed6
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-01-10 18:12:50 +0100

    Add v3 transactions; Update old ones to conform to new protocols

--- Sources/Starknet/Data/Transaction/Transaction.swift
+++ Sources/Starknet/Data/Transaction/Transaction.swift
@@ -1,7 +1,85 @@
 import BigInt
 import Foundation
 
-public struct StarknetInvokeTransactionV1: StarknetTransaction {
+public struct StarknetInvokeTransactionV3: StarknetInvokeTransaction, StarknetTransactionV3, StarknetExecutableTransaction {
+    public let type: StarknetTransactionType = .invoke
+
+    public let version: Felt
+
+    public let senderAddress: Felt
+
+    public let calldata: StarknetCalldata
+
+    public let signature: StarknetSignature
+
+    public let nonce: Felt
+
+    public let resourceBounds: StarknetResourceBoundsMapping
+
+    public let tip: UInt64AsHex
+
+    public let paymasterData: StarknetPaymasterData
+
+    public let accountDeploymentData: StarknetAccountDeploymentData
+
+    public let nonceDataAvailabilityMode: StarknetDAMode
+
+    public let feeDataAvailabilityMode: StarknetDAMode
+
+    public let hash: Felt?
+
+    public init(senderAddress: Felt, calldata: StarknetCalldata, signature: StarknetSignature, l1ResourceBounds: StarknetResourceBounds, nonce: Felt, forFeeEstimation: Bool = false, hash: Felt? = nil) {
+        self.senderAddress = senderAddress
+        self.calldata = calldata
+        self.signature = signature
+        self.nonce = nonce
+        self.version = StarknetInvokeTransactionV3.computeVersion(3, forFeeEstimation: forFeeEstimation)
+        self.hash = hash
+        // As of Starknet 0.13, most of v3 fields have hardcoded values.
+        self.resourceBounds = StarknetResourceBoundsMapping(l1Gas: l1ResourceBounds)
+        self.tip = .zero
+        self.paymasterData = []
+        self.accountDeploymentData = []
+        self.nonceDataAvailabilityMode = .l1
+        self.feeDataAvailabilityMode = .l1
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case version
+        case senderAddress = "sender_address"
+        case calldata
+        case signature
+        case nonce
+        case resourceBounds = "resource_bounds"
+        case tip
+        case paymasterData = "paymaster_data"
+        case accountDeploymentData = "account_deployment_data"
+        case nonceDataAvailabilityMode = "nonce_data_availability_mode"
+        case feeDataAvailabilityMode = "fee_data_availability_mode"
+        case hash = "transaction_hash"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.senderAddress = try container.decode(Felt.self, forKey: .senderAddress)
+        self.calldata = try container.decode(StarknetCalldata.self, forKey: .calldata)
+        self.signature = try container.decode(StarknetSignature.self, forKey: .signature)
+        self.nonce = try container.decode(Felt.self, forKey: .nonce)
+        self.version = try container.decode(Felt.self, forKey: .version)
+        self.resourceBounds = try container.decode(StarknetResourceBoundsMapping.self, forKey: .resourceBounds)
+        self.tip = try container.decode(UInt64AsHex.self, forKey: .tip)
+        self.paymasterData = try container.decode(StarknetPaymasterData.self, forKey: .paymasterData)
+        self.accountDeploymentData = try container.decode(StarknetAccountDeploymentData.self, forKey: .accountDeploymentData)
+        self.nonceDataAvailabilityMode = try container.decode(StarknetDAMode.self, forKey: .nonceDataAvailabilityMode)
+        self.feeDataAvailabilityMode = try container.decode(StarknetDAMode.self, forKey: .feeDataAvailabilityMode)
+        self.hash = try container.decodeIfPresent(Felt.self, forKey: .hash)
+
+        try verifyTransactionType(container: container, codingKeysType: CodingKeys.self)
+    }
+}
+
+public struct StarknetInvokeTransactionV1: StarknetInvokeTransaction, StarknetDeprecatedTransaction, StarknetExecutableTransaction {
     public let type: StarknetTransactionType = .invoke
 
     public let version: Felt
@@ -53,7 +131,7 @@ public struct StarknetInvokeTransactionV1: StarknetTransaction {
     }
 }
 
-public struct StarknetInvokeTransactionV0: StarknetTransaction {
+public struct StarknetInvokeTransactionV0: StarknetInvokeTransaction, StarknetDeprecatedTransaction {
     public let type: StarknetTransactionType = .invoke
 
     public let version: Felt = .zero
@@ -104,10 +182,85 @@ public struct StarknetInvokeTransactionV0: StarknetTransaction {
     }
 }
 
-// TODO: Remove when migrating to RPC 0.6.0
-public typealias StarknetDeployAccountTransaction = StarknetDeployAccountTransactionV1
+public struct StarknetDeployAccountTransactionV3: StarknetDeployAccountTransaction, StarknetTransactionV3, StarknetExecutableTransaction {
+    public let type: StarknetTransactionType = .deployAccount
 
-public struct StarknetDeployAccountTransactionV1: StarknetTransaction {
+    public let version: Felt
+
+    public let signature: StarknetSignature
+
+    public let nonce: Felt
+
+    public let contractAddressSalt: Felt
+
+    public let constructorCalldata: StarknetCalldata
+
+    public let classHash: Felt
+
+    public var resourceBounds: StarknetResourceBoundsMapping
+
+    public var tip: UInt64AsHex
+
+    public var paymasterData: StarknetPaymasterData
+
+    public var nonceDataAvailabilityMode: StarknetDAMode
+
+    public var feeDataAvailabilityMode: StarknetDAMode
+
+    public let hash: Felt?
+
+    public init(signature: StarknetSignature, l1ResourceBounds: StarknetResourceBounds, nonce: Felt, contractAddressSalt: Felt, constructorCalldata: StarknetCalldata, classHash: Felt, forFeeEstimation: Bool = false, hash: Felt? = nil) {
+        self.signature = signature
+        self.nonce = nonce
+        self.contractAddressSalt = contractAddressSalt
+        self.constructorCalldata = constructorCalldata
+        self.classHash = classHash
+        self.version = StarknetDeployAccountTransactionV3.computeVersion(3, forFeeEstimation: forFeeEstimation)
+        self.hash = hash
+        // As of Starknet 0.13, most of v3 fields have hardcoded values.
+        self.resourceBounds = StarknetResourceBoundsMapping(l1Gas: l1ResourceBounds)
+        self.tip = UInt64AsHex(0)
+        self.paymasterData = []
+        self.nonceDataAvailabilityMode = .l1
+        self.feeDataAvailabilityMode = .l1
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.signature = try container.decode(StarknetSignature.self, forKey: .signature)
+        self.nonce = try container.decode(Felt.self, forKey: .nonce)
+        self.contractAddressSalt = try container.decode(Felt.self, forKey: .contractAddressSalt)
+        self.constructorCalldata = try container.decode(StarknetCalldata.self, forKey: .constructorCalldata)
+        self.classHash = try container.decode(Felt.self, forKey: .classHash)
+        self.resourceBounds = try container.decode(StarknetResourceBoundsMapping.self, forKey: .resourceBounds)
+        self.tip = try container.decode(UInt64AsHex.self, forKey: .tip)
+        self.paymasterData = try container.decode([Felt].self, forKey: .paymasterData)
+        self.nonceDataAvailabilityMode = try container.decode(StarknetDAMode.self, forKey: .nonceDataAvailabilityMode)
+        self.feeDataAvailabilityMode = try container.decode(StarknetDAMode.self, forKey: .feeDataAvailabilityMode)
+        self.version = try container.decode(Felt.self, forKey: .version)
+        self.hash = try container.decodeIfPresent(Felt.self, forKey: .hash)
+
+        try verifyTransactionVersion(container: container, codingKeysType: CodingKeys.self)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case version
+        case signature
+        case nonce
+        case contractAddressSalt = "contract_address_salt"
+        case constructorCalldata = "constructor_calldata"
+        case classHash = "class_hash"
+        case resourceBounds = "resource_bounds"
+        case tip
+        case paymasterData = "paymaster_data"
+        case nonceDataAvailabilityMode = "nonce_data_availability_mode"
+        case feeDataAvailabilityMode = "fee_data_availability_mode"
+        case hash = "transaction_hash"
+    }
+}
+
+public struct StarknetDeployAccountTransactionV1: StarknetDeployAccountTransaction, StarknetDeprecatedTransaction, StarknetExecutableTransaction {
     public let type: StarknetTransactionType = .deployAccount
 
     public let version: Felt
@@ -210,7 +363,86 @@ public struct StarknetL1HandlerTransaction: StarknetTransaction {
     }
 }
 
-public struct StarknetDeclareTransactionV0: StarknetTransaction {
+public struct StarknetDeclareTransactionV3: StarknetDeclareTransaction, StarknetTransactionV3 {
+    public let type: StarknetTransactionType = .declare
+
+    public let version: Felt = 3
+
+    public let signature: StarknetSignature
+
+    public let classHash: Felt
+
+    public let senderAddress: Felt
+
+    public let nonce: Felt
+
+    public let resourceBounds: StarknetResourceBoundsMapping
+
+    public let tip: UInt64AsHex
+
+    public let paymasterData: StarknetPaymasterData
+
+    public let accountDeploymentData: StarknetAccountDeploymentData
+
+    public let nonceDataAvailabilityMode: StarknetDAMode
+
+    public let feeDataAvailabilityMode: StarknetDAMode
+
+    public let hash: Felt?
+
+    public init(signature: StarknetSignature, l1ResourceBounds: StarknetResourceBounds, nonce: Felt, classHash: Felt, senderAddress: Felt, hash: Felt? = nil) {
+        self.signature = signature
+        self.nonce = nonce
+        self.classHash = classHash
+        self.senderAddress = senderAddress
+        self.hash = hash
+        // As of Starknet 0.13, most of v3 fields have hardcoded values.
+        self.resourceBounds = StarknetResourceBoundsMapping(l1Gas: l1ResourceBounds)
+        self.tip = .zero
+        self.paymasterData = []
+        self.accountDeploymentData = []
+        self.nonceDataAvailabilityMode = .l1
+        self.feeDataAvailabilityMode = .l1
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.signature = try container.decode(StarknetSignature.self, forKey: .signature)
+        self.classHash = try container.decode(Felt.self, forKey: .classHash)
+        self.senderAddress = try container.decode(Felt.self, forKey: .senderAddress)
+        self.nonce = try container.decode(Felt.self, forKey: .nonce)
+        self.resourceBounds = try container.decode(StarknetResourceBoundsMapping.self, forKey: .resourceBounds)
+        self.tip = try container.decode(UInt64AsHex.self, forKey: .tip)
+        self.paymasterData = try container.decode(StarknetPaymasterData.self, forKey: .paymasterData)
+        self.accountDeploymentData = try container.decode(StarknetAccountDeploymentData.self, forKey: .accountDeploymentData)
+        self.nonceDataAvailabilityMode = try container.decode(StarknetDAMode.self, forKey: .nonceDataAvailabilityMode)
+        self.feeDataAvailabilityMode = try container.decode(StarknetDAMode.self, forKey: .feeDataAvailabilityMode)
+        self.hash = try container.decodeIfPresent(Felt.self, forKey: .hash)
+
+        try verifyTransactionType(container: container, codingKeysType: CodingKeys.self)
+
+        try verifyTransactionType(container: container, codingKeysType: Self.CodingKeys.self)
+        try verifyTransactionVersion(container: container, codingKeysType: Self.CodingKeys.self)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case version
+        case signature
+        case classHash = "class_hash"
+        case senderAddress = "sender_address"
+        case nonce
+        case resourceBounds = "resource_bounds"
+        case tip
+        case paymasterData = "paymaster_data"
+        case accountDeploymentData = "account_deployment_data"
+        case nonceDataAvailabilityMode = "nonce_data_availability_mode"
+        case feeDataAvailabilityMode = "fee_data_availability_mode"
+        case hash = "transaction_hash"
+    }
+}
+
+public struct StarknetDeclareTransactionV0: StarknetDeclareTransaction, StarknetDeprecatedTransaction {
     public let type: StarknetTransactionType = .declare
 
     public let maxFee: Felt
@@ -256,7 +488,7 @@ public struct StarknetDeclareTransactionV0: StarknetTransaction {
     }
 }
 
-public struct StarknetDeclareTransactionV1: StarknetTransaction {
+public struct StarknetDeclareTransactionV1: StarknetDeclareTransaction, StarknetDeprecatedTransaction {
     public let type: StarknetTransactionType = .declare
 
     public let maxFee: Felt
@@ -409,7 +641,7 @@ public enum StarknetTransactionDecodingError: Error {
     case invalidType
 }
 
-extension StarknetTransaction {
+extension StarknetExecutableTransaction {
     private static func estimateVersion(_ version: Felt) -> Felt {
         Felt(BigUInt(2).power(128).advanced(by: BigInt(version.value)))!
     }
