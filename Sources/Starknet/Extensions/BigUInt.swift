5b58e48646849c7a8b6dc465478d5561f1635331
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-01-15 12:02:45 +0100

    Add some extra BigUInt extensions

--- Sources/Starknet/Extensions/BigUInt.swift
+++ Sources/Starknet/Extensions/BigUInt.swift
@@ -10,6 +10,22 @@ public extension BigUInt {
         self > Felt.max.value ? Felt.max : Felt(self)!
     }
 
+    func toUInt64AsHex() -> UInt64AsHex? {
+        UInt64AsHex(self)
+    }
+
+    func toUInt64AsHexClamped() -> UInt64AsHex {
+        UInt64AsHex(clamping: self)
+    }
+
+    func toUInt128AsHex() -> UInt128AsHex? {
+        UInt128AsHex(self)
+    }
+
+    func toUInt128AsHexClamped() -> UInt128AsHex {
+        UInt128AsHex(clamping: self)
+    }
+
     func toNumAsHex() -> NumAsHex? {
         NumAsHex(self)
     }
