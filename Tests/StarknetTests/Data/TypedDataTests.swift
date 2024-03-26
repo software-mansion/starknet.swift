4bb69cc0a9d01969e4765ba3aedbf825bcb14aaf
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-03-26 19:34:00 +0100

    Add rev 1 test case
    - Rename test files `typed_data_[..].json`-> `typed_data_rev_0_[..].json`
    - Add `typed_data_rev_1_example.json`
    - Update `TypedDataTests`
      - Move old td cases to `CasesRev0`
      - Add `CasesRev1`

--- Tests/StarknetTests/Data/TypedDataTests.swift
+++ Tests/StarknetTests/Data/TypedDataTests.swift
@@ -8,11 +8,18 @@
 import XCTest
 
 final class TypedDataTests: XCTestCase {
-    static let td = loadTypedDataFromFile(name: "typed_data_example")!
-    static let tdFeltArr = loadTypedDataFromFile(name: "typed_data_felt_array_example")!
-    static let tdString = loadTypedDataFromFile(name: "typed_data_long_string_example")!
-    static let tdStructArr = loadTypedDataFromFile(name: "typed_data_struct_array_example")!
-    static let tdValidate = loadTypedDataFromFile(name: "typed_data_validate_example")!
+    enum CasesRev0 {
+        static let td = loadTypedDataFromFile(name: "typed_data_rev_0_example")!
+        static let tdFeltArr = loadTypedDataFromFile(name: "typed_data_rev_0_felt_array_example")!
+        static let tdString = loadTypedDataFromFile(name: "typed_data_rev_0_long_string_example")!
+        static let tdStructArr = loadTypedDataFromFile(name: "typed_data_rev_0_struct_array_example")!
+        static let tdValidate = loadTypedDataFromFile(name: "typed_data_rev_0_validate_example")!
+    }
+
+    enum CasesRev1 {
+        static let td = loadTypedDataFromFile(name: "typed_data_rev_1_example")!
+    }
+
     static let exampleDomainV0 = """
     {
         "name": "DomainV0",
@@ -57,15 +64,18 @@ final class TypedDataTests: XCTestCase {
 
     func testTypeHashCalculation() throws {
         let cases: [(StarknetTypedData, String, Felt)] = [
-            (Self.td, "StarkNetDomain", "0x1bfc207425a47a5dfa1a50a4f5241203f50624ca5fdf5e18755765416b8e288"),
-            (Self.td, "Person", "0x2896dbe4b96a67110f454c01e5336edc5bbc3635537efd690f122f4809cc855"),
-            (Self.td, "Mail", "0x13d89452df9512bf750f539ba3001b945576243288137ddb6c788457d4b2f79"),
-            (Self.tdString, "String", "0x1933fe9de7e181d64298eecb44fc43b4cec344faa26968646761b7278df4ae2"),
-            (Self.tdString, "Mail", "0x1ac6f84a5d41cee97febb378ddabbe1390d4e8036df8f89dee194e613411b09"),
-            (Self.tdFeltArr, "Mail", "0x5b03497592c0d1fe2f3667b63099761714a895c7df96ec90a85d17bfc7a7a0"),
-            (Self.tdStructArr, "Post", "0x1d71e69bf476486b43cdcfaf5a85c00bb2d954c042b281040e513080388356d"),
-            (Self.tdStructArr, "Mail", "0x873b878e35e258fc99e3085d5aaad3a81a0c821f189c08b30def2cde55ff27"),
-            (Self.tdValidate, "Validate", "0x2e86ac4735e6012fbeaa68cbd0e5a089014d0da150fa915769a35d5eba30593"),
+            (Self.CasesRev0.td, "StarkNetDomain", "0x1bfc207425a47a5dfa1a50a4f5241203f50624ca5fdf5e18755765416b8e288"),
+            (Self.CasesRev0.td, "Person", "0x2896dbe4b96a67110f454c01e5336edc5bbc3635537efd690f122f4809cc855"),
+            (Self.CasesRev0.td, "Mail", "0x13d89452df9512bf750f539ba3001b945576243288137ddb6c788457d4b2f79"),
+            (Self.CasesRev0.tdString, "String", "0x1933fe9de7e181d64298eecb44fc43b4cec344faa26968646761b7278df4ae2"),
+            (Self.CasesRev0.tdString, "Mail", "0x1ac6f84a5d41cee97febb378ddabbe1390d4e8036df8f89dee194e613411b09"),
+            (Self.CasesRev0.tdFeltArr, "Mail", "0x5b03497592c0d1fe2f3667b63099761714a895c7df96ec90a85d17bfc7a7a0"),
+            (Self.CasesRev0.tdStructArr, "Post", "0x1d71e69bf476486b43cdcfaf5a85c00bb2d954c042b281040e513080388356d"),
+            (Self.CasesRev0.tdStructArr, "Mail", "0x873b878e35e258fc99e3085d5aaad3a81a0c821f189c08b30def2cde55ff27"),
+            (Self.CasesRev0.tdValidate, "Validate", "0x2e86ac4735e6012fbeaa68cbd0e5a089014d0da150fa915769a35d5eba30593"),
+            (Self.CasesRev1.td, "StarknetDomain", "0x1ff2f602e42168014d405a94f75e8a93d640751d71d16311266e140d8b0a210"),
+            (Self.CasesRev1.td, "Person", "0x30f7aa21b8d67cb04c30f962dd29b95ab320cb929c07d1605f5ace304dadf34"),
+            (Self.CasesRev1.td, "Mail", "0x560430bf7a02939edd1a5c104e7b7a55bbab9f35928b1cf5c7c97de3a907bd"),
         ]
 
         try cases.forEach { data, typeName, expectedResult in
@@ -78,35 +88,41 @@ final class TypedDataTests: XCTestCase {
     func testStructHashCalculation() throws {
         let cases: [(StarknetTypedData, String, String, Felt)] = [
             (
-                Self.td,
+                Self.CasesRev0.td,
                 "StarkNetDomain",
                 "domain",
                 "0x54833b121883a3e3aebff48ec08a962f5742e5f7b973469c1f8f4f55d470b07"
             ),
-            (Self.td, "Mail", "message", "0x4758f1ed5e7503120c228cbcaba626f61514559e9ef5ed653b0b885e0f38aec"),
+            (Self.CasesRev0.td, "Mail", "message", "0x4758f1ed5e7503120c228cbcaba626f61514559e9ef5ed653b0b885e0f38aec"),
             (
-                Self.tdString,
+                Self.CasesRev0.tdString,
                 "Mail",
                 "message",
                 "0x1d16b9b96f7cb7a55950b26cc8e01daa465f78938c47a09d5a066ca58f9936f"
             ),
             (
-                Self.tdFeltArr,
+                Self.CasesRev0.tdFeltArr,
                 "Mail",
                 "message",
                 "0x26186b02dddb59bf12114f771971b818f48fad83c373534abebaaa39b63a7ce"
             ),
             (
-                Self.tdStructArr,
+                Self.CasesRev0.tdStructArr,
                 "Mail",
                 "message",
                 "0x5650ec45a42c4776a182159b9d33118a46860a6e6639bb8166ff71f3c41eaef"
             ),
             (
-                Self.tdValidate,
+                Self.CasesRev0.tdValidate,
                 "Validate",
                 "message",
                 "0x87ecd5622070667d2534fa83dd9b16f6cb497b42998d301e8df0ed5875d02d"
+            ),
+            (
+                Self.CasesRev1.td,
+                "StarknetDomain",
+                "domain",
+                "0x555f72e550b308e50c1a4f8611483a174026c982a9893a05c185eeb85399657"
             ),
         ]
 
@@ -124,29 +140,34 @@ final class TypedDataTests: XCTestCase {
     func testMessageHashCalculation() throws {
         let cases: [(StarknetTypedData, Felt, Felt)] = [
             (
-                Self.td,
+                Self.CasesRev0.td,
                 "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
                 "0x6fcff244f63e38b9d88b9e3378d44757710d1b244282b435cb472053c8d78d0"
             ),
             (
-                Self.tdString,
+                Self.CasesRev0.tdString,
                 "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
                 "0x691b977ee0ee645647336f01d724274731f544ad0d626b078033d2541ee641d"
             ),
             (
-                Self.tdFeltArr,
+                Self.CasesRev0.tdFeltArr,
                 "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
                 "0x30ab43ef724b08c3b0a9bbe425e47c6173470be75d1d4c55fd5bf9309896bce"
             ),
             (
-                Self.tdStructArr,
+                Self.CasesRev0.tdStructArr,
                 "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
                 "0x5914ed2764eca2e6a41eb037feefd3d2e33d9af6225a9e7fe31ac943ff712c"
             ),
             (
-                Self.tdValidate,
+                Self.CasesRev0.tdValidate,
                 "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
                 "0x28e38c1c65783abb40b871705095584b96bcbf1f80c8268a0659d074b3afd92"
+            ),
+            (
+                Self.CasesRev1.td,
+                "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+                "0x7f6e8c3d8965b5535f5cc68f837c04e3bbe568535b71aa6c621ddfb188932b8"
             ),
         ]
 
