e330ede93f329de682e02a842d5db010a4243758
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-03-27 18:01:05 +0100

    Move build logic to `build` method

--- Sources/Starknet/Data/MerkleTree.swift
+++ Sources/Starknet/Data/MerkleTree.swift
@@ -13,22 +13,23 @@ public struct MerkleTree {
         guard !leaves.isEmpty else {
             return nil
         }
+        (self.rootHash, self.branches) = Self.build(leafHashes: leafHashes, hashMethod: hashMethod)
+    }
 
+    private static func build(leafHashes: [Felt], hashMethod: HashMethod) -> (Felt, [[Felt]]) {
         var leaves = leafHashes
         var branches: [[Felt]] = []
 
         while leaves.count > 1 {
-            if leaves.count != leafHashes.count {
+            if leaves.count != leaves.count {
                 branches.append(leaves)
             }
-
             leaves = stride(from: 0, to: leaves.count, by: 2).map {
                 Self.hash(leaves[$0], ($0 + 1) < leaves.count ? leaves[$0 + 1] : .zero, hashMethod)
             }
         }
 
-        self.rootHash = leaves[0]
-        self.branches = branches
+        return (leaves[0], branches)
     }
 
     public static func hash(_ a: Felt, _ b: Felt, _ hashMethod: HashMethod) -> Felt {
