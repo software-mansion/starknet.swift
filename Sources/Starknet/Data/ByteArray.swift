590d346567c1f4b78dbfccd6b318359847b6ea20
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-03-29 18:19:32 +0100

    Add `StarknetByteArray`
    - Add `fromString` initializer
    - Add initializer from string literal
    - Add public `String.splitToShortStrings()` extension

---
+++ Sources/Starknet/Data/ByteArray.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+public struct StarknetByteArray: Equatable, Hashable, ExpressibleByStringLiteral {
+    let data: [Felt]
+    let pendingWord: Felt
+    let pendingWordLen: Int
+
+    public init?(data: [Felt], pendingWord: Felt, pendingWordLen: Int) {
+        self.data = data
+        self.pendingWord = pendingWord
+        self.pendingWordLen = pendingWordLen
+
+        guard self.data.allSatisfy({ $0.byteLength == 31 }),
+              self.pendingWordLen >= 0,
+              self.pendingWordLen < 31,
+              self.pendingWord.byteLength == self.pendingWordLen
+        else {
+            return nil
+        }
+    }
+
+    public init(fromString: String) {
+        let shortStrings = fromString.splitToShortStrings()
+        let encodedShortStrings = shortStrings.map { Felt.fromShortString($0)! }
+
+        if shortStrings.isEmpty || shortStrings.last!.count == 31 {
+            self.data = encodedShortStrings
+            self.pendingWord = .zero
+            self.pendingWordLen = 0
+        } else {
+            self.data = encodedShortStrings.dropLast()
+            self.pendingWord = encodedShortStrings.last!
+            self.pendingWordLen = shortStrings.last!.count
+        }
+    }
+}
+
+public extension StarknetByteArray {
+    typealias StringLiteralType = String
+
+    init(stringLiteral value: String) {
+        self.init(fromString: value)
+    }
+}
+
+public extension String {
+    func splitToShortStrings() -> [String] {
+        let maxLen = 31
+        return stride(from: 0, to: count, by: maxLen).map { index in
+            let startIndex = self.index(self.startIndex, offsetBy: index)
+            let endIndex = self.index(startIndex, offsetBy: maxLen, limitedBy: self.endIndex) ?? self.endIndex
+            return String(self[startIndex ..< endIndex])
+        }
+    }
+}
+
+private extension Felt {
+    var byteLength: Int {
+        (value.bitWidth + 7) / 8
+    }
+}
