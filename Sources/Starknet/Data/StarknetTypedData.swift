daf429b4f2fba4156c3d01e6a131bfd1a523e14a
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-04-04 13:32:44 +0200

    Make `revision` public

--- Sources/Starknet/Data/StarknetTypedData.swift
+++ Sources/Starknet/Data/StarknetTypedData.swift
@@ -62,7 +62,7 @@ public struct StarknetTypedData: Codable, Equatable, Hashable {
     public let domain: Domain
     public let message: [String: Element]
 
-    private var revision: Revision {
+    public var revision: Revision {
         domain.resolveRevision()!
     }
 
