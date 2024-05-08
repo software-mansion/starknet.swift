825c0bceeb7478e31df58288c74278c9d1f83c60
Fiiranek <jellybean@onet.eu>
2024-05-08 13:50:52 +0200

    Set `forResource` param to empty string

--- Tests/StarknetTests/Utils/DevnetClient/DevnetClient.swift
+++ Tests/StarknetTests/Utils/DevnetClient/DevnetClient.swift
@@ -262,7 +262,7 @@ func makeDevnetClient() -> DevnetClientProtocol {
                 throw DevnetClientError.missingResourceFile
             }
 
-            guard let toolVersionsPath = Bundle.module.path(forResource: "Scarb", ofType: "tool-versions") else {
+            guard let toolVersionsPath = Bundle.module.path(forResource: "", ofType: "tool-versions") else {
                 throw DevnetClientError.missingResourceFile
             }
 
