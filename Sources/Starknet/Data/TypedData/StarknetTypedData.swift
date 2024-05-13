c779b025a7b4dac3e5f4c532797c97fd4a5e391a
Fiiranek <jellybean@onet.eu>
2024-05-13 11:46:17 +0200

    Update `StarkNet` to `Starknet` ocurencies

--- Sources/Starknet/Data/TypedData/StarknetTypedData.swift
+++ Sources/Starknet/Data/TypedData/StarknetTypedData.swift
@@ -33,7 +33,7 @@ public enum StarknetTypedDataError: Error, Equatable {
 /// let typedDataString = """
 /// {
 ///     "types": {
-///         "StarkNetDomain": [
+///         "StarknetDomain": [
 ///             {"name": "name", "type": "shortstring"},
 ///             {"name": "version", "type": "shortstring"},
 ///             {"name": "chainId", "type": "shortstring"},
@@ -50,7 +50,7 @@ public enum StarknetTypedDataError: Error, Equatable {
 ///         ],
 ///     },
 ///     "primaryType": "Mail",
-///     "domain": {"name": "StarkNet Mail", "version": "1", "chainId": "1", "revision": 1},
+///     "domain": {"name": "Starknet Mail", "version": "1", "chainId": "1", "revision": 1},
 ///     "message": {
 ///         "from": {
 ///             "name": "Cow",
@@ -384,7 +384,7 @@ public struct StarknetTypedData: Codable, Equatable, Hashable {
 
     public func getMessageHash(accountAddress: Felt) throws -> Felt {
         try hashArray([
-            Felt.fromShortString("StarkNet Message")!,
+            Felt.fromShortString("Starknet Message")!,
             getStructHash(domain: domain),
             accountAddress,
             getStructHash(typeName: primaryType, data: message),
@@ -416,7 +416,7 @@ public extension StarknetTypedData {
 
         public var separatorName: String {
             switch try! resolveRevision() {
-            case .v0: "StarkNetDomain"
+            case .v0: "StarknetDomain"
             case .v1: "StarknetDomain"
             }
         }
