d73d3e0bc984b05c25dec76d2855b5cea3fff313
Fiiranek <jellybean@onet.eu>
2024-10-24 19:56:39 +0200

    Move `ContractStorageKey`

--- Sources/Starknet/Providers/StarknetProvider/JsonRpcParams.swift
+++ Sources/Starknet/Providers/StarknetProvider/JsonRpcParams.swift
@@ -121,16 +121,6 @@ struct GetStorageProofParams: Encodable {
     }
 }
 
-public struct ContractStorageKey: Encodable {
-    let contractAddress: Felt
-    let key: Felt
-
-    enum CodingKeys: String, CodingKey {
-        case contractAddress = "contract_address"
-        case key
-    }
-}
-
 struct GetTransactionByHashParams: Encodable {
     let hash: Felt
 
