c756ec33f77eb72839b713c626e46de8ed4cce92
Fiiranek <jellybean@onet.eu>
2025-04-14 00:54:47 +0200

    Apply code review suggestions

--- Sources/Starknet/Data/StorageProof/ContractsProof.swift
+++ Sources/Starknet/Data/StorageProof/ContractsProof.swift
@@ -7,16 +7,6 @@ public struct ContractsProof: Decodable, Equatable {
         case contractLeavesData = "contract_leaves_data"
     }
 
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        nodes = try container.decode(NodeHashToNodeMapping.self, forKey: .nodes)
-        contractLeavesData = try container.decode([ContractLeafData].self, forKey: .contractLeavesData)
-    }
-
-    public static func == (lhs: ContractsProof, rhs: ContractsProof) -> Bool {
-        lhs.nodes == rhs.nodes && lhs.contractLeavesData == rhs.contractLeavesData
-    }
-
     public struct ContractLeafData: Decodable, Equatable {
         public let nonce: Felt
         public let classHash: Felt
--- Sources/Starknet/Data/StorageProof/NodeHashToNodeMappingItem.swift
+++ Sources/Starknet/Data/StorageProof/NodeHashToNodeMappingItem.swift
@@ -9,13 +9,6 @@ public struct NodeHashToNodeMappingItem: Decodable, Equatable {
         case node
     }
 
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        nodeHash = try container.decode(Felt.self, forKey: .nodeHash)
-        node = try container.decode(MerkleNode.self, forKey: .node)
-    }
-
     public static func == (lhs: NodeHashToNodeMappingItem, rhs: NodeHashToNodeMappingItem) -> Bool {
         lhs.nodeHash == rhs.nodeHash && lhs.node == rhs.node
     }
