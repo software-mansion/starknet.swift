a929a35ab67df649002d407701981b4d1e35ad53
Dionysis Karatzas <diokaratzas@gmail.com>
2025-06-11 11:44:47 +0300

    Add init method to StarknetResourceBoundsMapping (#234)
    * Add init method to StarknetResourceBoundsMapping
    
    * Swiftformat

--- Sources/Starknet/Data/Transaction/Data/ResourceBounds.swift
+++ Sources/Starknet/Data/Transaction/Data/ResourceBounds.swift
@@ -31,6 +31,12 @@ public struct StarknetResourceBoundsMapping: Codable, Equatable, Hashable {
         l1DataGas: StarknetResourceBounds.zero
     )
 
+    public init(l1Gas: StarknetResourceBounds, l2Gas: StarknetResourceBounds, l1DataGas: StarknetResourceBounds) {
+        self.l1Gas = l1Gas
+        self.l2Gas = l2Gas
+        self.l1DataGas = l1DataGas
+    }
+
     enum CodingKeys: String, CodingKey {
         case l1Gas = "l1_gas"
         case l2Gas = "l2_gas"
